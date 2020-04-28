676a967eeaf00db094b39d7c61e400e8105753ab
Victor Maciel <victormaciel0216@gmail.com>
2020-04-28 06:27:21 -0300

    add spaceship

---
+++ Ship.js
@@ -0,0 +1,44 @@
+class Ship {
+  constructor(){
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.radius = SHIP_SIZE /2;
+    this.inclination = (90 / 180) * Math.PI; //90 degrees to radians
+    this.rotation = 0; 
+
+    this.update = this.update.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  update(){
+    this.inclination += this.rotation;
+  }
+
+  render(){
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.beginPath();
+    //ship nose
+    ctx.moveTo(
+      this.x + this.radius * Math.cos(this.inclination),
+      this.y - this.radius * Math.sin(this.inclination)
+    );
+    //rear left
+    ctx.lineTo(
+      this.x - this.radius * (Math.cos(this.inclination) + Math.sin(this.inclination)),
+      this.y + this.radius * (Math.sin(this.inclination) - Math.cos(this.inclination))
+    );
+    //rear center
+    ctx.lineTo(
+      this.x - this.radius * (2/3) * Math.cos(this.inclination),
+      this.y + this.radius * (2/3) * Math.sin(this.inclination)
+    );
+    //rear right
+    ctx.lineTo(
+      this.x - this.radius * (Math.cos(this.inclination) - Math.sin(this.inclination)),
+      this.y + this.radius * (Math.sin(this.inclination) + Math.cos(this.inclination))
+    );
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const FPS = 60; //frames per second
+const LINE_WIDTH = 2; //default line width for drawing
 
+const SHIP_SIZE = 30 ; //ship height in pixels
 
 /**@type {HTMLCanvasElement} */
 const canvas = document.querySelector("#gameCanvas");
 const ctx = canvas.getContext("2d");
+
+let ship = new Ship();
 
 //set up the game loop
 setInterval(() => {
@@ -13,11 +17,13 @@ setInterval(() => {
 
 function update(){
 
+  ship.update();
 }
 
 function render(){
   //draw background
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
+
+  ship.render();
 }
